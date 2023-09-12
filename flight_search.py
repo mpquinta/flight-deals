ecd7e235bf8e930cebec49d9aed58b1f2f6d74bc
mpquinta <meganquintana30@gmail.com>
2023-09-11 22:51:51 -0700

    finished 340

--- flight_search.py
+++ flight_search.py
@@ -1,46 +1,45 @@
-import requests 
+import requests
+from datetime import datetime, timedelta
 
 class FlightSearch():
     #This class is responsible for talking to the Flight Search API.
 
     def __init__(self):
-        pass
-        # self.iata_code = "TESTING - this is coming from the FLIGHT SEARCH class!"
+        self.headers = {
+            "apikey": "AP0yzH3RH3HUBhkt8TfjgPZHGdPNZ7X0"
+        }
 
     def search_iata_code(self, city):
         query_endpoint = "https://api.tequila.kiwi.com/locations/query"
-
-        headers = {
-            "apikey": "AP0yzH3RH3HUBhkt8TfjgPZHGdPNZ7X0"
-        }
 
         params = {
             "term": city
         }
 
-        get_response = requests.get(url=query_endpoint, headers=headers, params=params)
+        get_response = requests.get(url=query_endpoint, headers=self.headers, params=params)
         city_info = get_response.json()
         # print(city_info)
 
         return city_info["locations"][0]["code"]
 
+    def flight_search(self, iata_code):
+        search_endpoint = "https://api.tequila.kiwi.com/v2/search"
 
-        # endpoint = "https://api.tequila.kiwi.com/v2/search"
+        params = {
+            "fly_from": "SNA",
+            "fly_to": iata_code,
+            "date_from": (datetime.now() + timedelta(days=1)).strftime("%d/%m/%Y"),
+            "date_to": (datetime.now() + timedelta(days=181)).strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "USD"
+        }
 
-        # authorization_header = {
-        #     "Authorization": ""
-        # }
+        response = requests.get(url=search_endpoint, headers=self.headers, params=params)
+        result = response.json()
 
-        # params = {
-        #     "apikey": "AP0yzH3RH3HUBhkt8TfjgPZHGdPNZ7X0",
-        #     "fly_from": "SNA",
-        #     "fly_to": "SFO",
-        #     "date_from": "09/09/2023",
-        #     "date_to": "03/09/2023"
+        return result
 
-        # }
-
-        # response = requests.post(url=endpoint, headers=params)
-        # result = response.json()
-
-        # print(result)
--- main.py
+++ main.py
@@ -2,13 +2,22 @@
 
 import data_manager
 from flight_search import FlightSearch
+from pprint import pprint
 
 google_sheet_data = data_manager.DataManager()
 sheet_data = google_sheet_data.prices
 
+city = FlightSearch()
+
 for i in range(len(sheet_data["prices"])):
+    current_city = sheet_data["prices"][i]["city"]
     if sheet_data["prices"][i]["iataCode"] == '':
-        city = FlightSearch()
-        iata_code = city.search_iata_code(sheet_data["prices"][i]["city"])
-        # sheet_data["prices"][i]["iataCode"] = iata_code
+        iata_code = city.search_iata_code(current_city)
         google_sheet_data.update_iata_code(sheet_data["prices"][i]["id"], iata_code)
+
+    # pass the cities into city.flight_search
+    flight_search_results = city.flight_search(sheet_data["prices"][i]["iataCode"])
+    # print(flight_search_results)
+    flight_price = flight_search_results["data"][0]["price"]
+    # print(flight_price)
+    print(f"{current_city}: ${flight_price}")    
