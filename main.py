512a7a8f9b00bd94f57110b35828cded358ddfa7
mpquinta <meganquintana30@gmail.com>
2023-09-17 14:24:02 -0700

    completed

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 
 import data_manager
 from flight_search import FlightSearch
-import notification_manager
+from notification_manager import NotificationManager
 from pprint import pprint
 
 google_sheet_data = data_manager.DataManager()
 sheet_data = google_sheet_data.prices
-twilio = notification_manager.NotificationManager()
+twilio = NotificationManager()
 
 city = FlightSearch()
 
@@ -19,21 +19,29 @@ for i in range(len(sheet_data["prices"])):
 
     # pass the cities into city.flight_search
     flight_search_results = city.flight_search(sheet_data["prices"][i]["iataCode"])
-    pprint(flight_search_results)
+    # pprint(flight_search_results)
+    
+    #extract necessary info from the flight search results 
     from_city = flight_search_results["data"][0]["route"][0]["cityFrom"]
     from_iata = flight_search_results["data"][0]["route"][0]["flyFrom"]
     to_city = flight_search_results["data"][0]["route"][0]["cityTo"]
     to_iata = flight_search_results["data"][0]["route"][0]["flyTo"]
+    start_date_raw = flight_search_results["data"][0]["route"][0]["local_departure"].split("T")
+    start_date = start_date_raw[0]
+    end_date_raw = flight_search_results["data"][0]["route"][1]["local_departure"].split("T")
+    end_date = end_date_raw[0]
+    # print(from_city, from_iata, to_city, to_iata, start_date, end_date)
 
     # print the cheapest price tickets
     flight_price = flight_search_results["data"][0]["price"]
-    # print(f"{current_city}: ${flight_price}")
+    print(f"{current_city}: ${flight_price}")
 
     # check cheapest flight against price in google sheet
     desired_price = sheet_data["prices"][i]["lowestPrice"]
+    
+    print("desired price: ", desired_price)
     if flight_price < desired_price:
-        twilio.send_text(flight_price, )
-    # if price is cheaper than google sheet
-        #send a text
+        twilio.send_text(
+            message=f"Low price alert! Only ${flight_price} to fly from {from_city}-{from_iata} to {to_city}-{to_iata} from {start_date} to {end_date}.")
 
 
--- notification_manager.py
+++ notification_manager.py
@@ -4,18 +4,17 @@ from twilio.rest import Client
 # account_sid = os.environ['TWILIO_ACCOUNT_SID']
 # auth_token = os.environ['TWILIO_AUTH_TOKEN']
 account_sid = "ACa2638faacf0f43ca9b0bddc720ad534e"
-auth_token = "cf24ed4a816c27329c31a08887c75868"
-client = Client(account_sid, auth_token)
+auth_token = "9d8dacb1d45befce6e7ce4c3183f3c2c"
+
 
 class NotificationManager:
     def __init__(self):
-        pass
+        self.client = Client(account_sid, auth_token)        
 
     #This class is responsible for sending notifications with the deal flight details.
-    def send_text(self, price, from_city, from_iata, to_city, to_iata, start_date, end_date):
-        message = client.messages \
-                    .create(
-                        body=f"Low price alert! Only {price} to fly from {from_city}-{from_iata} to {to_city}-{to_iata} from {start_date} to {end_date}.",
+    def send_text(self, message):
+        message = self.client.messages.create(
+                        body=message,
                         from_='+18664412539',
                         to='+14158454243'
                     )
