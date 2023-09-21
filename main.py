e782aa03d5cdd9254920bad609878c33d02635e0
mpquinta <meganquintana30@gmail.com>
2023-09-20 23:22:15 -0700

    added exception

--- main.py
+++ main.py
@@ -19,29 +19,32 @@ for i in range(len(sheet_data["prices"])):
 
     # pass the cities into city.flight_search
     flight_search_results = city.flight_search(sheet_data["prices"][i]["iataCode"])
-    # pprint(flight_search_results)
-    
-    #extract necessary info from the flight search results 
-    from_city = flight_search_results["data"][0]["route"][0]["cityFrom"]
-    from_iata = flight_search_results["data"][0]["route"][0]["flyFrom"]
-    to_city = flight_search_results["data"][0]["route"][0]["cityTo"]
-    to_iata = flight_search_results["data"][0]["route"][0]["flyTo"]
-    start_date_raw = flight_search_results["data"][0]["route"][0]["local_departure"].split("T")
-    start_date = start_date_raw[0]
-    end_date_raw = flight_search_results["data"][0]["route"][1]["local_departure"].split("T")
-    end_date = end_date_raw[0]
-    # print(from_city, from_iata, to_city, to_iata, start_date, end_date)
+        # pprint(flight_search_results)
+    try:       
+        #extract necessary info from the flight search results 
+        from_city = flight_search_results["data"][0]["route"][0]["cityFrom"]
+        from_iata = flight_search_results["data"][0]["route"][0]["flyFrom"]
+        to_city = flight_search_results["data"][0]["route"][0]["cityTo"]
+        to_iata = flight_search_results["data"][0]["route"][0]["flyTo"]
+        start_date_raw = flight_search_results["data"][0]["route"][0]["local_departure"].split("T")
+        start_date = start_date_raw[0]
+        end_date_raw = flight_search_results["data"][0]["route"][1]["local_departure"].split("T")
+        end_date = end_date_raw[0]
+        # print(from_city, from_iata, to_city, to_iata, start_date, end_date)
 
-    # print the cheapest price tickets
-    flight_price = flight_search_results["data"][0]["price"]
-    print(f"{current_city}: ${flight_price}")
+        # print the cheapest price tickets
+        flight_price = flight_search_results["data"][0]["price"]
+        # print(f"{current_city}: ${flight_price}")
 
-    # check cheapest flight against price in google sheet
-    desired_price = sheet_data["prices"][i]["lowestPrice"]
+        # check cheapest flight against price in google sheet
+        desired_price = sheet_data["prices"][i]["lowestPrice"]
+        
+        # print("desired price: ", desired_price)
+        if flight_price < desired_price:
+            twilio.send_text(
+                message=f"Low price alert! Only ${flight_price} to fly from {from_city}-{from_iata} to {to_city}-{to_iata} from {start_date} to {end_date}.")
     
-    print("desired price: ", desired_price)
-    if flight_price < desired_price:
-        twilio.send_text(
-            message=f"Low price alert! Only ${flight_price} to fly from {from_city}-{from_iata} to {to_city}-{to_iata} from {start_date} to {end_date}.")
+    except IndexError or TypeError:
+        continue
 
 
